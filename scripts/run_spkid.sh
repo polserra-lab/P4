0aa4cbed6f11ccf148c4b22df732d55431f42b89
Pol Serra Montes <pol.serra.i.montes@estudiantat.upc.edu>
2022-11-28 16:01:13 +0100

    hola

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -22,6 +22,7 @@ w=work
 name_exp=one
 db_devel=spk_8mu/speecon
 db_test=spk_8mu/sr_test
+world=users
 
 # Ficheros de resultados del reconocimiento y verificación
 LOG_CLASS=$w/class_${FEAT}_${name_exp}.log
@@ -166,7 +167,9 @@ for cmd in $*; do
        # Implement 'trainworld' in order to get a Universal Background Model for speaker verification
        #
        # - The name of the world model will be used by gmm_verify in the 'verify' command below.
-       gmm_train  -v 1 -T 1e-5 -N 25 -m 64 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$world.gmm $lists/verif/$world.train || exit 1
+       
+       EXEC="gmm_train -v 1 -T 0.001 -N 5 -m 5 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/verif/$world.train"
+        echo $EXEC && $EXEC || exit 1
 
    elif [[ $cmd == verify ]]; then
        ## @file
@@ -177,8 +180,9 @@ for cmd in $*; do
        #   For instance:
        #   * <code> gmm_verify ... > $LOG_VERIF </code>
        #   * <code> gmm_verify ... | tee $LOG_VERIF </code>
-       (gmm_verify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm -w $world $lists/gmm.list  $lists/verif/all.test $lists/verif/all.test.candidates |
-        tee $w/verif_${FEAT}_${name_exp}.log) || exit 1
+       EXEC="gmm_verify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm -w $world $lists/gmm.list $lists/verif/all.test $lists/verif/all.test.candidates"
+        echo $EXEC && $EXEC | tee $LOG_VERIFY || exit 1
+       
 
    elif [[ $cmd == verifyerr ]]; then
        if [[ ! -s $LOG_VERIF ]] ; then
@@ -198,10 +202,8 @@ for cmd in $*; do
        #
        # El fichero con el resultado del reconocimiento debe llamarse $FINAL_CLASS, que deberá estar en el
        # directorio de la práctica (PAV/P4).
-
-       compute_$FEAT $db_verif $lists/final/class.test
-       (gmm_classify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm $lists/gmm.list  $lists/final/class.test | tee $w/class_test.res) || exit 1
-       perl -ane 'print "$F[0]\t$F[1]\n"' $w/class_test.res | tee class_test.log
+        echo "Implement"
+       
    
    elif [[ $cmd == finalverif ]]; then
        ## @file
@@ -220,11 +222,7 @@ for cmd in $*; do
        # candidato para la señal a verificar. En $FINAL_VERIF se pide que la tercera columna sea 1,
        # si se considera al candidato legítimo, o 0, si se considera impostor. Las instrucciones para
        # realizar este cambio de formato están en el enunciado de la práctica.
-       compute_$FEAT $db_verif $lists/final/verif.test 
-       (gmm_verify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm -w $world $lists/gmm.list  $lists/final/verif.test $lists/final/verif.test.candidates |
-        tee $w/verif_test.res) || exit 1
-        perl -ane 'print "$F[0]\t$F[1]\t"; if ($F[2] > 0.499060800203697) {print "1\n"} else {print "0\n"}' $w/verif_test.res | 
-        tee verif_test.log
+       echo "Implement"
    
    # If the command is not recognize, check if it is the name
    # of a feature and a compute_$FEAT function exists.
